d8d8ac4ebaa32e6adf4d6c8f0520c29091236e75
Dilan Tompkins <65461019+TomTerrific7@users.noreply.github.com>
2021-10-18 19:44:00 -0400

    almost complete

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -11,18 +11,20 @@ const Projects = () => {
                 
             }}
         >
-            <h1>Projects
+            <h2>Projects<hr color="black" size="2" width="200" align="center"></hr>
             <h5>DecentraLearn:<h6>Only decentralized, permission-less application on the Ethereum network to broadcast educational information about any token and get people incentivized while learning about it. Uses Chainlink Keepers, Openeppelin RBAC, IPFS, React.
             <br/>Awarded Compound Finance Grant, Chanlink prize pool award, and Pocket Network Prize pool award.</h6>
             </h5>
+            <hr color="black" size="2" width="800" align="center"></hr>
             <h5>Russian Roulette:<h6>Simple Russian Roulette game on the Ethereum blockchain that uses Chainlink VRF every round until a winner is chosen.
             <br/>Final Project in Chainshot Ethereum Bootcamp</h6>
             </h5>
+            <hr color="black" size="2" width="800" align="center"></hr>
             <h5>Chainlink-Lottery:<h6>Simple Lottery on the Ethereum blockchain using Chainlink VRF and Chainlink Keepers. 
             <br/>This is still an ongoing project.</h6>
             </h5>
             
-            </h1>
+            </h2>
             
             
             
